8036cd6aa97668830f3b3bbfca63c8c301841a8c
Su <suelagozz@gmail.com>
2026-01-07 00:00:06 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,52 +4,65 @@
 #include "encrypt.h"
 #include "file_ops.h"
 
-
-int verify_master(char *out_master);
-
 int main() {
-    char master[100];
-
- 
+    char master[256];
+    
+    printf(" PASSWORD VAULT \n");
+    
+    
     if (!verify_master(master)) {
-        printf("Vault locked.\n");
+        printf("\nAccess denied. Vault locked.\n");
         return 0;
     }
-
+    
+    printf("\nAccess granted!\n");
+    
     init_crypto(master);
-
+    
+    memset(master, 0, sizeof(master));
+    
     int choice;
-
     do {
-        printf("\n= PASSWORD VAULT =\n");
-        printf("1 - Add new password\n");
-        printf("2 - View saved passwords\n");
-        printf("3 - Delete password\n");
-        printf("4 - Exit\n");
-        printf("Choice: ");
-
-        scanf("%d", &choice);
+     
+        printf("           PASSWORD VAULT                  \n");
+        printf("===========================================\n");
+        printf("  1 - Add new password                     \n");
+        printf("  2 - View saved passwords                 \n");
+        printf("  3 - Delete password                      \n");
+        printf("  4 - Exit                                 \n");
+       
+        printf("\nChoice: ");
+        
+        if (scanf("%d", &choice) != 1) {
+            while (getchar() != '\n');
+            printf("Invalid input! Please enter a number.\n");
+            continue;
+        }
         while (getchar() != '\n');
-
+        
         switch (choice) {
         case 1:
+            printf("\n>>> ADD NEW PASSWORD\n");
             add_password();
             break;
         case 2:
+            printf("\n>>> VIEW PASSWORDS\n");
             view_passwords();
+            printf("Press Enter to continue...");
+            getchar();
             break;
         case 3:
+            printf("\n>>> DELETE PASSWORD\n");
             delete_password();
             break;
         case 4:
-            printf("Goodbye.\n");
+            printf("\nVault locked. Goodbye!\n");
             break;
         default:
-            printf("Invalid option.\n");
+            printf("\nInvalid option. Choose 1-4.\n");
         }
+        
     } while (choice != 4);
-
+    
     return 0;
 }
-
-
